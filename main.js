cf0dfa2de3a32911fbcec3c69b7ee4d7024dd768
eonOzux <yohanarce@gmail.com>
2023-08-12 16:05:06 -0600

    Second commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var character = ['a' ,'b', 'c', 'd','e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o',
+ 'p', 'q', 'r', 's', 't', 'i', 'u', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var spec_character = ['*', '/', '-', '+', ':', '?', '&', '%', '^', '#', '@', '!'];
+const combinedValues = character + spec_character; 
+const randomValues = [];
+
+document.write(randomSelected);
+var password = '';
+
+function passGen(password,randomValues) {
+
+    password = character + spec_character;
+    console.log(i);
+};
+
+
+// probando codigo de chat 
+
+
+// Ejemplo de uso
