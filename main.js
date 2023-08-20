abc628506af41d7e083fe4b2bcf3a6e8b8062fb8
eonOzux <yohanarce@gmail.com>
2023-08-19 20:14:26 -0600

    Visuals commit

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
-// probando codigo de chat 
-
-// Pide al usuario la longitud deseada para la contraseña
+function generate(){
+// Ask the user the length of the password.
 const passwordLength = parseInt(prompt('How many characters would you like your new password to be?'));
 
-// Definir los caracteres posibles para la contraseña
+// Define the possible characters in the password.
 const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
-// Variable para almacenar la contraseña generada
+// Password variable.
 var newPassword = '';
 
-// Generar la contraseña usando un ciclo for
+// Generate the password with a for cicle.
 for (let i = 0; i < passwordLength; i++) {
     // Generar un índice aleatorio para seleccionar un carácter de la lista
     const randomIndex = Math.floor(Math.random() * characters.length);
     
-    // Agregar el carácter seleccionado a la contraseña
+    // Add the selected characters to the variable
     newPassword += characters.charAt(randomIndex);
-// Escribe la contraseña en la pagina
+
 }
 
-// Imprimir la contraseña generada
-console.log('Your new password is:', newPassword);
+// Write the password in the document
 document.write('Your new password is: ', newPassword);
-
-// Ejemplo de uso
+};
